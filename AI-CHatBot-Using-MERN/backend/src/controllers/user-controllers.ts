dd9e04aa34ef31b15007d9ada9bbf87dcd1b678a
BlackBoxJU50 <ahmedhasib2224@gmail.com>
2025-09-09 02:44:11 +0600

    Model Creation

--- AI-CHatBot-Using-MERN/backend/src/controllers/user-controllers.ts
+++ AI-CHatBot-Using-MERN/backend/src/controllers/user-controllers.ts
@@ -1,4 +1,11 @@
-export const getAllUsers = () => {
-
-    
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ Message: "OK", users });
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({ Message: "ERROR", cause: error.message });
+  }
 };
